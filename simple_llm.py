dd1e149c7d01df396498aef44770fdd0b19f0534
Rendy Christian <78911479+rendychristiann@users.noreply.github.com>
2024-03-06 23:18:20 +0700

    Update simple_llm.py

--- simple_llm.py
+++ simple_llm.py
@@ -2,7 +2,7 @@ import os
 from langchain_openai import ChatOpenAI
 import gradio as gr
 # Memasukkan API key
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "YOUR_OPENAI_API_KEY"
 gpt3 = ChatOpenAI(model_name="gpt-3.5-turbo" )
 def chatbot(prompt):
     return gpt3.invoke(prompt).content
